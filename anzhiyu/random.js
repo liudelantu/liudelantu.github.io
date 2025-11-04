91958f1a6940d5e6f6108dd7fb1bd1b7b211ce3f
afeng <liudelantu@gmail.com>
2025-11-04 13:29:17 +0800

    Site updated: 2025-11-04 13:29:16

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2025/11/03/github+hexo部署属于自己的博客/github+hexo部署属于自己的博客/","2025/11/03/hello-world/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
